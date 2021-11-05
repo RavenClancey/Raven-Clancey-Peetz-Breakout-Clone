4b72a9592c674d3bddbb8e0ce0b81fc62b202a8b
RavenClancey <RavenClancey1@gmail.com>
2021-11-05 18:58:51 +1300

    Main Game done

--- Raven Clancey-Peetz Breakout Clone/Assets/Scripts/BallScript.cs
+++ Raven Clancey-Peetz Breakout Clone/Assets/Scripts/BallScript.cs
@@ -4,25 +4,67 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
+    [SerializeField] float maxSpeed = 20;
+    [SerializeField] float startSpeed = 5;
     [SerializeField] float xSpeed = 0;
     [SerializeField] float ySpeed = 0;
+    [SerializeField][Range(1, 2)] float speedModifier = 1.05f;
+
+    private bool ballActive = false;
+    private GameObject paddleObject;
+
+    [SerializeField] Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        xSpeed = 0;
-        ySpeed = -1;
+     
+        if(paddleObject == null)
+        {
+            paddleObject = GameObject.Find("Player");
+        }
+
+        if (rb == null)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        // Add the x speed and y speed to the players current position 
-        transform.position = new Vector3(transform.position.x + xSpeed, transform.position.y + ySpeed, 0);
+
+        if (ballActive)
+        {
+            // Add the x speed and y speed to the players current position 
+            rb.MovePosition(new Vector3(transform.position.x + xSpeed * Time.deltaTime, transform.position.y + ySpeed * Time.deltaTime, 0));
+        }
+        else
+        {
+            rb.MovePosition(new Vector3(paddleObject.transform.position.x, paddleObject.transform.position.y + 1.0f, 0));
+
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                ySpeed = startSpeed;
+                xSpeed = Random.Range(-startSpeed, startSpeed);
+             
+                ballActive = true;
+            }
+        }
+      
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        
+        if (Mathf.Abs(ySpeed) < maxSpeed )
+        {
+           
+            xSpeed *= speedModifier;
+            ySpeed *= speedModifier;
+        }
 
+        
         switch(collision.gameObject.tag)
         {
             case "Wall":
@@ -37,10 +79,51 @@ public class BallScript : MonoBehaviour
                     break;
                 }
 
+            case "DeadZone":
+                {
+                    ballActive = false;
+                    break;
+                }
+
+            case "Brick":
+                {
+                    // disable brick that is hit 
+                    collision.gameObject.SetActive(false);
+
+                    //different check needed for top and bottom otherwise if you hit 2 bricks at once it * -1 twice and it doesnt flip
+
+                    print(collision.contacts[0].normal);
+                    // Bounce off bottom of brick
+                    if(collision.contacts[0].normal.y < 0 )
+                    {
+                        ySpeed = -Mathf.Abs(ySpeed);
+                    }
+
+                    // Bounce off top of brick
+                    if (collision.contacts[0].normal.y > 0)
+                    {
+                        ySpeed = Mathf.Abs(ySpeed);
+                    }
+
+                    // Bounce off side of brick
+                    if (collision.contacts[0].normal.x != -0)
+                    {
+                        xSpeed *= -1;
+                    }
+
+                    break;
+                }
+
             case "Paddle":
                 {
+                    //flip Y speed & make X Speed equal the difference of ball pos and paddle pos.
                     ySpeed *= -1;
+                    
+                    // This makes the ball bounce at a shaper angle at the edges of the paddle
+                    xSpeed = transform.position.x - collision.gameObject.transform.position.x;
+                    xSpeed *= Mathf.Abs((ySpeed / 3.0f) );
                     break;
+                    
                 }
             default:
                 {
--- Raven Clancey-Peetz Breakout Clone/Assets/Scripts/PlayerScript.cs
+++ Raven Clancey-Peetz Breakout Clone/Assets/Scripts/PlayerScript.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //find and attach rigid body if not done already
+        if(rb == null)
+        {
+            rb = GetComponent<Rigidbody>();
+        }
     }
 
     // Update is called once per frame
@@ -17,23 +23,23 @@ public class PlayerScript : MonoBehaviour
         
 
         if(Input.GetKey(KeyCode.A) )
-        {
-            transform.position = new Vector3(transform.position.x - (moveSpeed * Time.deltaTime), transform.position.y, 0);
+        { 
+            rb.MovePosition(new Vector3(transform.position.x - (moveSpeed * Time.deltaTime), transform.position.y, 0));
         }
 
         if (Input.GetKey(KeyCode.D) )
-        {
-            
-            transform.position = new Vector3(transform.position.x + (moveSpeed * Time.deltaTime), transform.position.y, 0);
+        {          
+            rb.MovePosition(new Vector3(transform.position.x + (moveSpeed * Time.deltaTime), transform.position.y, 0));
         }
 
-        if(transform.position.x > 11.5f)
+        if(transform.position.x > 14.5f)
         {
-            transform.position = new Vector3(11.5f, transform.position.y, 0);
+            
+            rb.MovePosition(new Vector3(14.5f, transform.position.y, 0));
         }
-        if (transform.position.x < -11.5f)
+        if (transform.position.x < -14.5f)
         {
-            transform.position = new Vector3(-11.5f, transform.position.y, 0);
+            rb.MovePosition(new Vector3(-14.5f, transform.position.y, 0));
         }
 
     }
